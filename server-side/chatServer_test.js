252a58243d86b77791781cb1d26012620593bf8a
jeremy3000 <shggreen@hotmail.com>
2014-08-09 04:23:40 +0900

    yaaa

--- server-side/chatServer_test.js
+++ server-side/chatServer_test.js
@@ -14,6 +14,12 @@ var loggedIn;
 
 io.sockets.on('connection', function (socket){
 	
+	var arr = [];
+	arr[0] = 23599685197365248;
+	arr[1] = 23593455197365248;
+
+	console.log(JSON.stringify(arr));
+
 	socket.on('signup', function (data){
 		
 		checkid(data, socket);
@@ -21,10 +27,7 @@ io.sockets.on('connection', function (socket){
 	
 	});
 	
-	socket.on('login', function (type, data){	
-		
-		console.log('yo');		
-		console.log(data);
+	socket.on('login', function (type, data){
 
 		if(type == 1)
 			login(data, socket);
@@ -84,7 +87,8 @@ function login(data, socket)
 		{
 			db.query('SELECT * FROM UserInfo WHERE userid="' + data.userid + '"', function(error2, userinfo){
 				socket.emit('loginSuccess', userinfo[0]);
-			});			
+				showRooms(data.userid, socket);
+			});				
 		}
 		else
 		{
@@ -109,13 +113,40 @@ function fblogin(data, socket)
 		else
 		{
 			socket.emit('loginSuccess', userinfo[0]);
-		}
+		}	
+
+		showRooms(data.id, socket);
 
 	});
 }
 
 function createRoom(data, socket)
 {
+	db.query('select UUID_SHORT()', function (error, room_id){
+		db.query('SELECT * FROM Roomlist WHERE id=' + room_id, function (error, response) {
+			if(response.length == 0)
+			{
+				db.query('insert into Roomlist values ( (select UUID_SHORT()), "' + data.room_name + '", ' + data.isPrivate + ', "' + data.room_password + '")');
+			}
+			else
+			{
+				createRoom(data, socket);
+			}
+		});
+	});
+	
 	
 }
-	
+
+function showRooms(userid, socket)
+{
+	db.query('SELECT * FROM UserInfo WHERE userid="' + userid + '"', function(error, userinfo){
+		var parsed = JSON.parse(userinfo[0].joined_room);		
+		socket.emit('showRooms', parsed);
+	});
+}
+
+function enterRoom(data, socket)
+{
+
+}
